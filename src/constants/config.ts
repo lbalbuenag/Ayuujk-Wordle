c3467b9b66de5906f9d770c6000707897c907081
lbalbuenag <lbalbuena7@gmail.com>
2022-10-13 11:33:02 -0500

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -4,8 +4,8 @@ export const CONFIG = {
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
   author: 'lbalbuenag', // Put your name here so people know who made this Wordle!
   authorWebsite: 'http://ayuujkjaaky.tlahuitoltepec.com/', // Put a link to your website or social media here
-  wordListSource: 'YourDictionarySource', // Describe the source material for your words here
-  wordListSourceLink: 'YourDictionaryLink', // Put a link to the source material for your words here
+  wordListSource: 'Appyuujk', // Describe the source material for your words here
+  wordListSourceLink: 'http://ayuujkjaaky.tlahuitoltepec.com/index.php/ahorcado', // Put a link to the source material for your words here
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
   //
