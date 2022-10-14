5c9d25fb39257803165ce1c48e2ce16526c962ec
lbalbuenag <lbalbuena7@gmail.com>
2022-10-13 20:04:06 -0500

    Cambios generales de configuración

--- src/constants/validGuesses.ts
+++ src/constants/validGuesses.ts
@@ -1,7 +1,7 @@
 import { CONFIG } from './config'
 
 export const VALIDGUESSES = [
-"kaaky","nitky","kë'xp","wääjx","tiixy","käjpy","xejty","kutpy","ko'on","yuu'n","kujup","najan","kujan","kë'ëk","xuumy"
+"kaaky","nitky","kë'xp","wääjx","tiixy","käjpy","xejty","kutpy","ko'on","yuu'n","kujup","najan","kujan","kë'ëk","xuumy","japom","kiixy","nëjkx"
 ]
 
 if (CONFIG.normalization) {
