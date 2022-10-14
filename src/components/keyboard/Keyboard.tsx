bb54796fe7bd71a97c2609b4e8adf1812b358230
lbalbuenag <lbalbuena7@gmail.com>
2022-10-13 20:33:49 -0500

    Uso de teclado

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -35,9 +35,7 @@ export const Keyboard = ({ onChar, onDelete, onEnter, guesses }: Props) => {
       }
       // Take away key event listener for now
       else {
-        const key = e.key//.toUpperCase()
-        console.log(e);
-        console.log(ORTHOGRAPHY);
+        const key = e.key.toUpperCase()
         if (key.length === 1 && ORTHOGRAPHY.indexOf(key) > -1) {
           onChar(key)
         }
